3a70bff2acbd5ecb4c8ff750787795341b3b4b93
김용일 <minerkyi@gmail.com>
2025-06-04 17:53:51 +0900

    25/06/04 수업 내용

--- react/20250530/react19/src/index.js
+++ react/20250530/react19/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+// import App from "./App2";
+import TodoApp from "./App3";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<TodoApp />);
