161f544217294180d55d2b929e07c55f1cbe81d1
김용일 <minerkyi@gmail.com>
2025-06-16 13:17:41 +0900

    25/06/16 수업 내용

---
+++ webpack/0616/src/app.js
@@ -0,0 +1,9 @@
+import plus from "./plus";
+import './app.css';
+import tiger from './tiger.png';
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.innerHTML = `<img src="${tiger}"/>`;
+});
+
+console.log(plus(2, 3));
